bb3c9382d01ac3e9decdbeeac5ce7b66de086275
Tigran Hakobyan <tik.hakobyan@gmail.com>
2018-07-22 09:19:51 -0400

    Comment main js file.

--- src/main.js
+++ src/main.js
@@ -6,21 +6,21 @@ import moment from "moment";
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
-  el: "#root",
-  methods: {
-    test: function(e) {
-      console.log("test", e);
-    }
-  },
-  components: {
-    VuejsHeatmap
-  },
-  computed: {
-    customStartDate() {
-      return moment("2018-03-16")
-        .endOf("day")
-        .toDate();
-    }
-  }
-});
+// new Vue({
+//   el: "#root",
+//   methods: {
+//     test: function(e) {
+//       console.log("test", e);
+//     }
+//   },
+//   components: {
+//     VuejsHeatmap
+//   },
+//   computed: {
+//     customStartDate() {
+//       return moment("2018-03-16")
+//         .endOf("day")
+//         .toDate();
+//     }
+//   }
+// });
